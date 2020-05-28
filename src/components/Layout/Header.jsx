77f9159cda654869c771e505123ea856a7c6311b
Jesus Gonzalez <jesus.gonzalez.eu@gmail.com>
2020-05-28 13:01:58 +0200

    layout/header: responsive and styling

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,79 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import styles from './Header.module.scss'
 
-export const Header = () => (
-  <nav className={`${styles.wrapper} navbar`}>
-    <span className="navbar-brand">
-      <img
-        alt="logo"
-        className={styles.logo}
-        src="/logo.jpg"
-      />
+export const Header = () => {
+  const location = useLocation()
 
-      <Link to="/">Illum</Link>
-    </span>
-  </nav>
-)
+  const [shouldShow, setShouldShow] = React.useState(false)
+  const toggleShow = () => setShouldShow(!shouldShow)
+
+  const getNavItemClass = (path) => [
+    'nav-item',
+    path === location.pathname && 'active'
+  ].filter(k => k).join(' ')
+
+  const collapseClass = React.useMemo(
+    () => [
+      'collapse navbar-collapse',
+      shouldShow && 'show'
+    ].filter(k => k).join(' '),
+    [shouldShow]
+  )
+
+  return (
+    <nav className={`${styles.wrapper} navbar navbar-expand-lg navbar-light`}>
+      <span className="navbar-brand">
+        <img
+          alt="logo"
+          className={styles.logo}
+          src="/logo.jpg"
+        />
+
+        <Link className="navbar-brand" to="/">Illum</Link>
+      </span>
+
+      <button className="navbar-toggler" type="button" onClick={toggleShow}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className={collapseClass} id="illum-header-navbar">
+        <ul className="navbar-nav mr-auto">
+          <li
+            className={getNavItemClass('/table')}
+          >
+            <Link
+              className="nav-link"
+              to="/table"
+            >
+              DataTable
+            </Link>
+          </li>
+
+          <li
+            className={getNavItemClass('/chart')}
+          >
+            <Link
+              className="nav-link"
+              to="/chart"
+            >
+              Chart
+            </Link>
+          </li>
+
+          <li
+            className={getNavItemClass('/import')}
+          >
+            <Link
+              className="nav-link"
+              to="/import"
+            >
+              Import
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
